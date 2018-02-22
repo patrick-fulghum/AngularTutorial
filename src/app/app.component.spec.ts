c9d825d10fe8ef443fcf061fa71ecaad5fef5486
Patrick Fulghum <patrick.c.fulghum@gmail.com>
2018-02-22 17:32:47 -0500

    Properly import and export components, included changes makes all tests pass

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { FormsModule } from '@angular/forms';
 
 describe('AppComponent', () => {
@@ -8,7 +9,7 @@ describe('AppComponent', () => {
     TestBed.configureTestingModule({
       imports: [FormsModule],
       declarations: [
-        AppComponent, HeroesComponent
+        AppComponent, HeroesComponent, HeroDetailComponent
       ],
     }).compileComponents();
   }));
